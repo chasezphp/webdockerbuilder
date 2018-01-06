04c813a01134cdaddea234038deb6160e239870b
learnbyseven <dockergiri@hotmail.com>
2018-01-06 03:15:47 -0500

    final

--- index.php
+++ index.php
@@ -13,34 +13,25 @@
 <fieldset>
 <label><h3>Modules & options</h3></label>
 </fieldset>
-<h3>BASEIMAGE</h3>
-<select id="baseimage" name="baseimage" style="width: 200px; height: 30px; font-size: 20px; padding-top: 10px;">
- <option value="centos:6">centos:6</option>
- <option value="centos:7">centos:7</option>
- <option value="ubuntu:latest">ubuntu:latest</option>
- <option value="busybox:latest">busybox:latest</option>
-</select>
-<br>
-</br>
 <h3>MODULES</h3>
-<select id="module" name="module" style="width: 200px; height: 30px; font-size: 20px; padding-top: 10px;">
+<select id="module" name="module" style="width: 200px; height: 30px; font-size: 20px;">
  <option value="apache">apache</option>
  <option value="nginx">nginx</option>
  <option value="mariadb">mariadb</option>
  <option value="java">java</option>
- <option value="php">php</option>
+ <option value="phpapache">php</option>
 </select>
 <br>
 </br>
 
 
 <h3>OUTPUT IMAGE NAME</h3>
- <input style="width: 200px; height: 30px; font-size: 20px; padding-top: 10px; type="text" name="imagename">
+ <input style="width: 200px; height: 30px; font-size: 20px; type="text" name="imagename">
 <br>
 </br>
 
 <h3>IMAGE TAG</h3>
- <input style="width: 200px; height: 30px; font-size: 20px; padding-top: 10px; type="text" name="imagetag">
+ <input style="width: 200px; height: 30px; font-size: 20px; type="text" name="imagetag">
 <br>
 </br>
 
